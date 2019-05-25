60fa704c5ff4e2fda3b531456e496324ea1e1d57
DMdavydov <dmitry_davydov1@epam.com>
2019-05-25 21:59:18 +0300

    refactoring to crud interface. closes #10

--- pet-clinic-data/src/main/java/com/davydov/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/davydov/sfgpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.davydov.sfgpetclinic.services;
 
 import com.davydov.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
