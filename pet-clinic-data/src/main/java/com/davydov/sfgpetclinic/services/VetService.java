8d4bc0484f8fbaa9eac10c25d25d556991421e6b
DMdavydov <dmitry_davydov1@epam.com>
2019-05-25 19:44:54 +0300

    Impl Services.Closes #5

---
+++ pet-clinic-data/src/main/java/com/davydov/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.davydov.sfgpetclinic.services;
+
+import com.davydov.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
