6c0daf909f0588440026ac720735eec647fceeb6
DMdavydov <dmitry_davydov1@epam.com>
2019-06-14 21:34:56 +0300

    Added Pets and Contact info to owners startup. Closes #33

--- pet-clinic-data/src/main/java/com/davydov/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/davydov/sfgpetclinic/services/map/OwnerMapService.java
@@ -1,13 +1,24 @@
 package com.davydov.sfgpetclinic.services.map;
 
 import com.davydov.sfgpetclinic.model.Owner;
+import com.davydov.sfgpetclinic.model.Pet;
 import com.davydov.sfgpetclinic.services.OwnerService;
+import com.davydov.sfgpetclinic.services.PetService;
+import com.davydov.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerMapService(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -21,8 +32,29 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+        if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        if (pet.getPetType().getId() == null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    } else {
+                        throw new RuntimeException("Pet Type is required");
+                    }
+
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        } else {
+            return null;
+        }
     }
+
 
     @Override
     public void delete(Owner object) {
