0392b9f55575d4abf9cca76682dceb3d61087428
DMdavydov <dmitry_davydov1@epam.com>
2019-06-22 22:35:15 +0300

    JPA for vets. Closes #41

---
+++ pet-clinic-data/src/main/java/com/davydov/sfgpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,46 @@
+package com.davydov.sfgpetclinic.services.springdatajpa;
+
+import com.davydov.sfgpetclinic.model.Vet;
+import com.davydov.sfgpetclinic.repositories.SpecialtyRepository;
+import com.davydov.sfgpetclinic.repositories.VetRepository;
+import com.davydov.sfgpetclinic.services.VetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository, SpecialtyRepository specialtyRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
